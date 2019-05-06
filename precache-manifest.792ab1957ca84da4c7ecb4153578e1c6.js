964a9ca75f45531abaceb22844c7ee91c34e7990
Chang Liu <chang.liu@moblab.com>
2019-05-06 13:55:07 -0700

    Updates

---
+++ precache-manifest.792ab1957ca84da4c7ecb4153578e1c6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "cfd2efa665e51700740e2ed7861fcb3c",
+    "url": "/static/media/sara_qr.cfd2efa6.jpeg"
+  },
+  {
+    "revision": "4ea2a2c4bdb25e215d2c3ab5dfe7a88d",
+    "url": "/static/media/logo2.4ea2a2c4.jpeg"
+  },
+  {
+    "revision": "3dd261696db8fb4edeb50f76a8eff3be",
+    "url": "/static/media/logo1.3dd26169.jpeg"
+  },
+  {
+    "revision": "38c1157d193f83b3f45d9913a6403d7a",
+    "url": "/static/media/ios_qr.38c1157d.png"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d4471adbad21356dc2f3",
+    "url": "/static/js/main.54ea30e9.chunk.js"
+  },
+  {
+    "revision": "9a993174c854dd08587a",
+    "url": "/static/js/2.4d716320.chunk.js"
+  },
+  {
+    "revision": "d4471adbad21356dc2f3",
+    "url": "/static/css/main.5cca1660.chunk.css"
+  },
+  {
+    "revision": "c73510d5def06a78e04384264c62320f",
+    "url": "/index.html"
+  }
+];
